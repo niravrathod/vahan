6735be3cb05cf4b373c8404f3a12ccc4b53c4799
niravrathod <niravrathod473@gmail.com>
2018-11-16 11:06:48 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# vahan
+# parivahan-py
+
+
+## Get Vehicle Details from Registration Number from parivahan.gov site
+
+
+### how to install
+
+```bash
+
+pip install -e git+https://github.com/loanzen/parivahan-py.git#egg=parivahan
+
+```
+
+
+### how to use
+
+You can call the function for getting the registration details
+like this
+
+```python
+
+from parivahan import get_parivahan_data
+
+print get_parivahan_data('XX00XX0000')
+
+```
+
+to get vehicle stolen status
+
+```python
+
+from parivahan import is_vehicle_stolen
+
+print is_vehicle_stolen('XX00XX0000')
+
+```
+
+
+* Free software: MIT license
+
+
+Credits
+---------
+
+This package was created with Cookiecutter_ and the `audreyr/cookiecutter-pypackage`_ project template.
+
+.. _Cookiecutter: https://github.com/audreyr/cookiecutter
+.. _`audreyr/cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+requirements = [
+    "mechanize",
+    "bs4",
+    "lxml",
+    "get-captcha"
+]
+
+setup(
+    name='parivahan-py',
+    version='0.1.6',
+    description="Get Vehicle Details from Registration Number from parivahan.gov site",
+    long_description=None,
+    author="Suraj Arya",
+    author_email='suraj@loanzen.in',
+    url='https://github.com/suraj-arya/parivahan',
+    packages=[
+        'parivahan',
+    ],
+    package_dir={'parivahan':
+                 'parivahan'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="MIT license",
+    zip_safe=False,
+    keywords='parivahan',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7'
+    ]
+)
